fc08d7973d396ff67219ea9dcc7fd6c6d74ed6a4
whutbd <774592164@qq.com>
2017-04-27 18:39:21 +0800

    Update airdb_cli_main.cc

--- sdk/airdb_cli_main.cc
+++ sdk/airdb_cli_main.cc
@@ -1,11 +1,12 @@
-#include <iostream>
 #include "airdb_sdk.h"
 #include "leveldb/db.h"
+#include "timer.h"
 #include <gflags/gflags.h>
+#include <iostream>
 #include <boost/lexical_cast.hpp>
 #include "common/tprinter.h"
 #include <iostream>
-#include "timer.h"
+
 
 using airdb::AirDBSdk;
 
@@ -24,8 +25,6 @@ int main(int argc, char** argv) {
     //int64_t start_time = 0;
     std::vector<std::string> members;
     AirDBSdk::ParseFlagFromArgs(argc, argv, members);
-    int64_t zz_end_time = common::get_micros();
-    std::cout << "sdk init time used:" << (zz_end_time - start_time) / 1000 << "(ms)" << std::endl;
     AirDBSdk sdk(members);
     if (FLAGS_airdb_cmd == "show") {
     	common::TPrinter cprinter(7);
@@ -34,11 +33,8 @@ int main(int argc, char** argv) {
 	std::vector<airdb::NodeInfo> cluster_info;
 	sdk.ShowClusterStatus(cluster_info);
 	if (cluster_info.size() == 0) {
-	    std::cout << "=== size is 0 "<< std::endl;
 	    return -1;
 	}
-	std::cout << "cluster status size is " << cluster_info.size() << std::endl;
-	//LOG(INFO, "cluster status size is [%d]", cluster_info.size());
 	std::vector<airdb::NodeInfo>::iterator it;
 	for (it = cluster_info.begin(); it != cluster_info.end(); ++it) {
 	    std::string s_status = airdb::AirDBSdk::StatusToString(it->status);
@@ -54,39 +50,13 @@ int main(int argc, char** argv) {
         std::string key = FLAGS_airdb_key;
         std::string value = FLAGS_airdb_value;
         if (sdk.Put(key, value) == 0) {
-            std::cout << "put success" << std::endl;
-            //LOG(INFO, "put key[%s], value[%s] success", key.c_str(), value.c_str());
+            LOG(INFO, "put key[%s], value[%s] success", key.c_str(), value.c_str());
         } else {
-            std::cout << "put failed" << std::endl;
-            //LOG(INFO, "put key[%s], value[%s] failed", key.c_str(), value.c_str());
+           LOG(WARN, "put key[%s], value[%s] failed", key.c_str(), value.c_str());
         }
     } else if (FLAGS_airdb_cmd == "get") {
         std::string key = FLAGS_airdb_key;
         std::string value;
-        std::cout << "get key is " << key << std::endl;
-        if (FLAGS_airdb_target_addr != "addr") {
-            //get value by leveldb
-            std::cout << "==== airdb_target_addr is " << FLAGS_airdb_target_addr << std::endl;
-            //std::string data_path = "data/" + FLAGS_airdb_target_addr + "/@db";
-            //std::string data_path = "data/127.0.1.1\:8881@db";
-            std::string data_path = "/tmp/testdb";
-            leveldb::DB* db_ptr = NULL;
-            leveldb::Options options;
-            options.create_if_missing = true;
-            //leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db_ptr);
-            leveldb::Status status = leveldb::DB::Open(options, "/root/ps/bin/data/127.0.1.1:8881/@db", &db_ptr);
-            assert(status.ok()); 
-            /*if (db_ptr == NULL){
-                std::cout << "data is=" << data_path << "=" << std::endl;
-                std::cout << "leveldb open failed " << std::endl;
-                //return -1;
-            }*/
-            db_ptr->Get(leveldb::ReadOptions(), key, &value);
-            std::cout << "get target addr:" << data_path << ";value is " << value << std::endl;
-            //std::cout << "get target addr:" << FLAGS_airdb_target_addr << ";value is " << value << std::endl;
-            return 0;
-        }
-        start_time = common::get_micros();
         if (sdk.Get(key, &value) == 0) {
             std::cout << "key:" << key  <<";value:" << value << std::endl;
         } else {
